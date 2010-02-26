655222d99c8b579d88e450711229f0ad165dab46
Jan <jan.h.xie@gmail.com>
2010-02-26 16:01:12 +0800

    couch insert bench using couch restful api

---
+++ couch-insert.rb
@@ -0,0 +1,91 @@
+require 'couch'
+
+server = Couch::Server.new("localhost", "5984")
+
+if ARGV.size == 1 && ARGV[0] == "init"
+
+#    server.delete("/test")
+    server.put("/test/", "")
+    puts "news database cleared."
+
+    # db.setindex("ID", RDBTBL::ITDECIMAL | RDBTBL::ITKEEP)
+
+#    db.setindex("UserID", RDBTBL::ITDECIMAL | RDBTBL::ITKEEP)
+#    puts "index for UserID created."
+#
+#    db.setindex("CategoryID", RDBTBL::ITDECIMAL | RDBTBL::ITKEEP)
+#    puts "index for CategoryID created."
+#
+#    db.setindex("CreateTime", RDBTBL::ITDECIMAL | RDBTBL::ITKEEP)
+#    puts "index for CreateTime created."
+#
+#    db.setindex("Tags", RDBTBL::ITTOKEN | RDBTBL::ITKEEP)
+#    puts "index for Tags created."
+
+    puts "initialize completed."
+    exit
+end
+
+all_tags = [];
+chars = ('a' .. 'z')
+chars.each { |x|
+    chars.each { |y|
+        chars.each { |z|
+            all_tags << x + y + z
+        }
+    }
+}
+
+if ARGV.size == 2
+    begin_cat_id = ARGV[0].to_i
+    end_cat_id = ARGV[1].to_i
+else
+    begin_cat_id = 1
+    end_cat_id = 10000
+end
+
+tag_index = 0;
+id = (begin_cat_id - 1) * 55 + 1
+user_count = 10000
+begin_seconds = Time.mktime(2010, 1, 1).to_i # (1 Jan 2001)
+insert_count = 0
+
+start_time = Time.now
+puts start_time.strftime("%H:%M:%S - 0 record - 0 second")
+
+(begin_cat_id .. end_cat_id).each do |cat_id| # 20000 × 55 = 1,100,000
+    count = (cat_id % 10 + 1) * 10
+    count.times do
+        title = "This is title of news #{id}"
+        create_time = Time.at(begin_seconds + id)
+        user_id = cat_id % user_count
+        source = "source of news #{id}"
+        source_url = "http://github.com/janx"
+        status = 1
+
+        tags = ["Tag_#{cat_id % 3}_#{id % 5}"];
+        tag_count = id % 5 + 1
+        tag_count.times do
+            tags << all_tags[tag_index % all_tags.size]
+            tag_index = tag_index + 1
+        end
+
+        doc = <<-JSON
+          {"ID":"#{id}", "Title":"#{title}", "CategoryID":"#{cat_id}", "UserID":"#{user_id}", "Tags":"#{tags.join(" ")}", "Source":"#{source}", "SourceUrl":"#{source_url}", "Status":"#{status}"}
+        JSON
+        server.put("/test/#{id}", doc)
+
+        insert_count = insert_count + 1
+        id = id + 1
+
+        if (insert_count % (10000) == 0)
+            end_time = Time.now
+            interval = (end_time.to_f - start_time.to_f).round
+            puts end_time.strftime("%H:%M:%S - #{insert_count} records - #{interval}s")
+        end
+    end
+end
+
+end_time = Time.now
+interval = (end_time.to_f - start_time.to_f).round
+puts end_time.strftime("%H:%M:%S - #{insert_count} records - #{interval}s (completed)")
